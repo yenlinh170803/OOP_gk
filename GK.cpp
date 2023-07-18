ccb91515bdab3a26520f51fe8f5f9dfc743ac488
qinnei <101728303+qinnei@users.noreply.github.com>
2023-07-18 09:50:26 +0700

    class HopDong, BienChe

--- GK.cpp
+++ GK.cpp
@@ -1,4 +1,3 @@
-//#include<bits/stdc.h++>
 #include<iostream>
 #include<string>
 #include<iomanip>
@@ -6,38 +5,182 @@ using namespace std;
 
 class NhanVien
 {
+protected:
 	string maso;
 	string hoten;
-	float luongcb;
+	int luongcb;
 public:
 	//get-set
-	float getLuongcb(){return luongcb;}
-	NhanVien(string _hoten="hoten",string _maso="maso",float _luongcb=0)
-	{
-		hoten=_hoten;
-		maso=_maso;
-		luongcb=_luongcb;
-	}
-	NhanVien(const NhanVien&nv)
-	{
-		hoten=nv.hoten;
-		maso=nv.maso;
-		luongcb=nv.luongcb;
-	}
-	~NhanVien(){};
+	int getLuongCoBan();
+	//khoi tao-huy
+	NhanVien(string _hoten="hoten",string _maso="maso",int _luongcb=0);
+	NhanVien(const NhanVien&nv);
+	~NhanVien();
 	//ham truu tuong
-	virtual double getTinhLuong()=0;
+	virtual int getTinhLuong()=0;
 	virtual string getLoaiNV()=0;
+	//phuong thuc
+	void Nhap();
+	void Xuat();
 	
-	void Xuat()
-	{
-		cout<<"Loai nhan vien: "<<getLoaiNV()<<endl;
-		cout<<"Ma nhan vien: "<<maso<<endl;
-		cout<<"Ho ten:"<<hoten<<endl;
-	}
+};
+class BienChe:public NhanVien
+{
+    protected:
+    int hesoluong, tienphucap;
+    public:
+    //get-set
+    int getHeSoLuong();
+    int getTienPhuCap();
+    //khoi tao-huy
+    BienChe(string _hoten="hoten",string _maso="maso",int _luongcb=0,
+            int _hesoluong=0, int _tienphucap=0);
+	BienChe(const BienChe&bc);
+	~BienChe();
 	
+	virtual int getTinhLuong();
+	virtual string getLoaiNV();
+	void Nhap();
+	void Xuat();
+};
+class HopDong:public NhanVien
+{
+    protected:
+    int tiencong,songay;
+    float hesovuotgio;
+    public:
+    HopDong(string _hoten="hoten",string _maso="maso",int _luongcb=0,
+            int _tiencong=0, int _songay=0,float _hesovuotgio=0);
+	HopDong(const HopDong&h);
+	~HopDong();
+	virtual int getTinhLuong();
+	virtual string getLoaiNV();
+	void Nhap();
+	void Xuat();
 };
 int main()
 {
+    BienChe nv;
+    nv.Nhap();
+    nv.Xuat();
+    // HopDong hd;
+    // hd.Nhap();
+    // hd.Xuat();
 	return 0;
+}
+
+/***************HOP DONG***************/
+HopDong::HopDong(string _hoten,string _maso,int _luongcb,
+    int _tiencong, int _songay,float _hesovuotgio):NhanVien(_hoten,_maso,_luongcb)
+{
+    tiencong=_tiencong;
+    songay=_songay;
+    hesovuotgio=_hesovuotgio;
+}
+HopDong::HopDong(const HopDong&h)
+{
+    tiencong=h.tiencong;
+    songay=h.songay;
+    hesovuotgio=h.hesovuotgio;
+}
+HopDong::~HopDong(){};
+int HopDong::getTinhLuong(){
+    if (songay>22)
+    {
+        return luongcb+(songay-22)*hesovuotgio*tiencong;   
+    }
+    return luongcb;
+}
+string HopDong::getLoaiNV(){
+    return "HD";
+}
+void HopDong::Nhap(){
+    NhanVien::Nhap();
+    cin>>tiencong>>songay>>hesovuotgio;
+}
+void HopDong::Xuat(){
+    NhanVien::Xuat();
+    cout<<"Tien cong lao dong: "<<tiencong;
+    cout<<"\nSo ngay lam viec trong thang: "<<songay;
+    cout<<"\nHe so vuot gio: "<<hesovuotgio;
+    cout<<"\nTong luong: "<<getTinhLuong();
+}
+/***************BIEN CHE***************/
+//get-set
+int BienChe::getHeSoLuong()
+{
+    return hesoluong;
+}
+int BienChe::getTienPhuCap()
+{
+    return tienphucap;
+}
+//khoi tao-huy
+BienChe::BienChe(string _hoten,string _maso,int _luongcb,
+int _hesoluong, int _tienphucap):NhanVien(_hoten,_maso,_luongcb)
+{
+    hesoluong=_hesoluong;
+    tienphucap=_tienphucap;
+}
+BienChe::BienChe(const BienChe&bc):NhanVien(bc)
+{
+    hesoluong=bc.hesoluong;
+    tienphucap=bc.tienphucap;
+}
+BienChe::~BienChe(){};
+//phuong thuc
+int BienChe::getTinhLuong()
+{
+    return luongcb*hesoluong+tienphucap;
+}
+void BienChe::Nhap()
+{
+    NhanVien::Nhap();
+    cin>>hesoluong>>tienphucap;
+}
+string BienChe::getLoaiNV()
+{
+    return "BC";    
+}
+void BienChe::Xuat()
+{
+    NhanVien::Xuat();
+    cout<<"He so luong: "<<hesoluong;
+    cout<<"\nTien phu cap chuc vu: "<<tienphucap;
+    cout<<"\nTong luong: "<<getTinhLuong();
+}
+/***************NHAN VIEN***************/
+//get-set
+int NhanVien::getLuongCoBan() 
+{ 
+    return luongcb; 
+}
+//khoi tao-huy
+NhanVien::NhanVien(string _hoten, string _maso, int _luongcb)
+{
+	hoten = _hoten;
+	maso = _maso;
+	luongcb = _luongcb;
+}
+NhanVien::NhanVien(const NhanVien& nv)
+{
+	hoten = nv.hoten;
+	maso = nv.maso;
+	luongcb = nv.luongcb;
+}
+NhanVien::~NhanVien() {};
+//phuong thuc
+void NhanVien::Nhap()
+{
+    cin>>maso;
+    cin.ignore();
+    getline(cin,hoten);
+    cin>>luongcb;
+}
+void NhanVien::Xuat()
+{
+	cout << "Loai nhan vien: " << getLoaiNV() << '\n';
+	cout << "Ma nhan vien: " << maso << '\n';
+	cout << "Ho ten: " << hoten << '\n';
+	cout << "Muc luong co ban: " << luongcb << '\n';
 }
